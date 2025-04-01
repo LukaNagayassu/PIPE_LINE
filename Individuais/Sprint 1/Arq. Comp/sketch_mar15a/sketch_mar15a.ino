44f436f53bf7a1bfacdf21a1a867c8ae850493c5
felipeferronogueira <felipe.nogueira@sptech.school>
2025-04-01 14:33:36 -0300

    Organizando repositorio

---
+++ Individuais/Sprint 1/Arq. Comp/sketch_mar15a/sketch_mar15a.ino
@@ -0,0 +1,29 @@
+#include "Ultrasonic.h" // Puxando a biblioteca Ultrasonic.h
+
+const int PINO_TRIGGER = 12; // Esta definindo trigger como uma constante (valor fixo)
+const int PINO_ECHO = 13; // Recebe o sinal mandado pelo Trigger
+float distanciaMax = 200.0;
+float distanciaMin = 20.0;
+
+HC_SR04 sensor(PINO_TRIGGER, PINO_ECHO); // Criando a classe HC_SR04 com os pinos de Trigger e Echo
+
+void setup() {
+  Serial.begin(9600); // Configuração e inicialização da comunicação serial a 9600 bps
+}
+
+void loop() {
+  float distancia = sensor.distance(); // Leitura da distância do sensor
+
+  // Envia as variáveis no formato 'nome=valor', necessário para o Serial Plotter
+  Serial.print("DistanciaMax:"); // Espaço entre os valores
+  Serial.print(distanciaMax);
+  Serial.print(" ");
+  Serial.print("Distancia:"); // Espaço entre os valores
+  Serial.print(distancia); // Valor da distância
+  Serial.print(" ");
+  Serial.print("DistanciaMin:"); // Espaço entre os valores
+  Serial.println(distanciaMin); // Envia o valor mínimo
+
+
+  delay(1000); // Pausa de 1 segundo antes de uma nova leitura
+}
