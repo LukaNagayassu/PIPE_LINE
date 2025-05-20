5d6a6d03005b16d360eae5f8ad9c381d2861d065
felipeferronogueira <felipe.nogueira@sptech.school>
2025-05-20 14:41:43 -0300

    alterando login pro suporte

--- Site/web-data-viz/src/controllers/usuarioController.js
+++ Site/web-data-viz/src/controllers/usuarioController.js
@@ -24,6 +24,7 @@ function autenticar(req, res) {
                             email: resultadoAutenticar[0].email,
                             nome: resultadoAutenticar[0].nome,
                             senha: resultadoAutenticar[0].senha,
+                            cargo: resultadoAutenticar[0].cargo,
                             fkEmpresa: resultadoAutenticar[0].fkEmpresa
                         });     
 
